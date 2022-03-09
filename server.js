bc3f78502e532aa9515d5da39654ee104af6ecd9
shawasty <shawasty@gmail.com>
2022-03-08 23:47:51 -0500

    created and joined my first html to project

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const db = require('./db');
 const dotenv = require("dotenv");
+const path = require('path');
+
+
+//start connecting to html 
+const static_path = path.join(__dirname, "./public")
+
+
+app.use(express.static(static_path))
 
 const UserRoute = require('./routes/userRoute');
 const AuthRoute = require('./routes/auth');
@@ -18,9 +26,12 @@ app.use("/api/meals", MealRoute);
 app.use("/api/carts", CartRoute);
 app.use("/api/orders", OrderRoute);
 
+app.get("/",(req, res)=>{
+    res.send("This is the root")
+})
 
 
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 
 app.listen(PORT , () => console.log(`listening on port ${PORT}`) )
